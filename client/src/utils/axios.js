4298027274809da66e0a238d30ab2e07647a6e33
Vishal Singh <jashanchoudhary430@gmail.com>
2026-09-11 14:22:32 +0530

    Update production API URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://eventzo-1-2c7m.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://eventzo-1-ab77.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
